a1e89b9d8efe3775a9193b8a2b6285d9f03937e4
paramaguru077 <gurusivags07@gmail.com>
2024-07-17 22:25:03 +0530

    context Api

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ContextApi Practice/App'
+import App from './ContextApi/App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import Context from './ContextApi/Context'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
-  
-    <App />
+   <Context>
+   <App />
+   </Context>
+   
   
 
   </BrowserRouter>
